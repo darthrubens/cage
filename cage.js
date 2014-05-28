cb335617849efff784093166dec41bbf58f744cd
Jaquín Núñez <contacto@joaquinnunez.cl>
2014-05-28 14:18:15 -0400

    First set of photos

--- cage.js
+++ cage.js
@@ -1,7 +1,5 @@
 $(document).ready( function(){
-	
-	var n = Math.floor((Math.random()*19)+1);
+	var n = Math.floor((Math.random()*4)+1);
 	var cage = "url('img/cage-" + n + ".jpg')";
 	$("#cage").css('background-image', cage);
-
 })
